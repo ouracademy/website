d9af2fffd0bf0a43dd78d625c816dc6832097a2c
artmadeit <this.arthur.email@gmail.com>
2018-12-25 18:04:14 -0500

    feat: add POC start line component

---
+++ src/components/start-line.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StartLine = styled.p`
+  font-weight: bold;
+
+  :first-letter {
+    font-size: 72px;
+    float: left;
+    padding: 10px;
+    background-color: #7f7664;
+    margin-right: 10px;
+    color: white;
+    border-radius: 5px;
+    line-height: 70px;
+  }
+`;
+
+export default StartLine;
