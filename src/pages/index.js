f82bc21ac7885a51d4b92e296fb81a390012d5e3
artmadeit <this.arthur.email@gmail.com>
2018-12-25 16:15:52 -0500

    feat: add slugs to posts pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Layout from "../components/layout";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 
 const Title = styled.h1`
   display: inline-block;
@@ -12,28 +12,43 @@ const PostHeader = styled.h3`
   margin-bottom: 1rem;
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
+
+const PostTitle = styled.span`
+  font-size: 1.5rem;
+`;
+
 export default ({ data }) => (
   <Layout>
     <div>
       <Title>Amazing Pandas Eating Things</Title>
       <h4>{data.allMdx.totalCount} Posts</h4>
       {data.allMdx.edges.map(({ node }) => (
-        <div key={node.id}>
-          <PostHeader>
-            {node.frontmatter.title}{" "}
-            <span
-              style={{
-                color: "#bbb"
-              }}
-            >
-              — {node.frontmatter.date}
-            </span>
-          </PostHeader>
-          <p>{node.excerpt}</p>
-        </div>
+        <PostItem key={node.id} {...node} />
       ))}
     </div>
   </Layout>
+);
+
+const PostItem = ({ fields, frontmatter, excerpt }) => (
+  <div>
+    <PostHeader>
+      <StyledLink to={`posts/${fields.slug}`}>
+        <PostTitle>{frontmatter.title}</PostTitle>
+      </StyledLink>{" "}
+      <span
+        style={{
+          color: "#bbb"
+        }}
+      >
+        — {frontmatter.date}
+      </span>
+    </PostHeader>
+    <p>{excerpt}</p>
+  </div>
 );
 
 export const query = graphql`
@@ -43,6 +58,9 @@ export const query = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
