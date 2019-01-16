ee81743da8c59f5ce83dbcddc7b13f86d1bfaa2b
qpdian <qpdiam@gmail.com>
2019-01-16 12:58:51 -0500

    feat: add locale to date in post list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 import Layout from "../components/layout";
-import { graphql, Link } from "gatsby";
-import { Heading, Box, Button, Image } from "grommet";
+import {
+  graphql,
+  Link
+} from "gatsby";
+import {
+  Heading,
+  Box,
+  Button,
+  Image
+} from "grommet";
 
-const PostHeader = styled.h3`
+const PostHeader = styled.h3 `
   margin-bottom: 1rem;
 `;
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(Link)
+`
   text-decoration: none;
   color: inherit;
 `;
 
-const PostTitle = styled.span`
+const PostTitle = styled.span `
   font-size: 1.5rem;
 `;
 
-const Banner = () => (
-  <Box
-    pad="large"
-    align="center"
-    gap="medium"
-    direction="row-responsive"
-    fill="horizontal"
-    border="all"
-  >
-    <Box height="small" width="small" basis="1/4">
-      <Image
-        fit="contain"
-        src="/icon.png"
-        alt="Our academy logo"
-        style={{ borderRadius: "50%" }}
-      />
-    </Box>
-    <Box basis="auto">
-      <Heading size="medium">
-        Un lugar donde aprender desarrollo de software
-      </Heading>
-      <Heading level="3">Para ti, para mi, para todos</Heading>
-    </Box>
-  </Box>
+const Banner = () => ( <
+  Box pad = "large"
+  align = "center"
+  gap = "medium"
+  direction = "row-responsive"
+  fill = "horizontal"
+  border = "all" >
+  <
+  Box height = "small"
+  width = "small"
+  basis = "1/4" >
+  <
+  Image fit = "contain"
+  src = "/icon.png"
+  alt = "Our academy logo"
+  style = {
+    {
+      borderRadius: "50%"
+    }
+  }
+  /> <
+  /Box> <
+  Box basis = "auto" >
+  <
+  Heading size = "medium" >
+  Un lugar donde aprender desarrollo de software <
+  /Heading> <
+  Heading level = "3" > Para ti, para mi, para todos < /Heading> <
+  /Box> <
+  /Box>
 );
 
-const Posts = styled.div`
+const Posts = styled.div `
   display: grid;
   grid-gap: 5px;
 
@@ -75,49 +90,87 @@ const Posts = styled.div`
   }
 `;
 
-export default ({ data }) => (
-  <Layout>
-    <div>
-      <Banner />
-      <Heading level="4" size="large">
-        <Button label={data.allMdx.totalCount} /> Posts
-      </Heading>
-      {/* {data.allMdx.edges.map(({ node }) => (
-      <PostItem key={node.id} {...node} />
-      
-      ))} */}
-      <Posts>
-        {data.allMdx.edges.map(({ node }) => (
+export default ({
+  data
+}) => ( <
+  Layout >
+  <
+  div >
+  <
+  Banner / >
+  <
+  Heading level = "4"
+  size = "large" >
+  <
+  Button label = {
+    data.allMdx.totalCount
+  }
+  /> Posts <
+  /Heading> {
+    /* {data.allMdx.edges.map(({ node }) => (
           <PostItem key={node.id} {...node} />
-        ))}
-      </Posts>
-    </div>
-  </Layout>
+          
+          ))} */
+  } <
+  Posts > {
+    data.allMdx.edges.map(({
+      node
+    }) => ( <
+      PostItem key = {
+        node.id
+      } { ...node
+      }
+      />
+    ))
+  } <
+  /Posts> <
+  /div> <
+  /Layout>
 );
 
-const PostItem = ({ fields, frontmatter, excerpt }) => (
-  <Box elevation="medium" pad="medium">
-    <PostHeader>
-      <StyledLink to={`posts${fields.slug}`}>
-        <PostTitle>{frontmatter.title}</PostTitle>
-      </StyledLink>{" "}
-      <span
-        style={{
-          color: "#bbb"
-        }}
-      >
-        — {frontmatter.date}
-      </span>
-    </PostHeader>
-    {frontmatter.image ? (
-      <Image src={frontmatter.image} fit="cover" />
-    ) : (
-      <p>{excerpt}</p>
-    )}
-  </Box>
+const PostItem = ({
+  fields,
+  frontmatter,
+  excerpt
+}) => ( <
+  Box elevation = "medium"
+  pad = "medium" >
+  <
+  PostHeader >
+  <
+  StyledLink to = {
+    `posts${fields.slug}`
+  } >
+  <
+  PostTitle > {
+    frontmatter.title
+  } < /PostTitle> <
+  /StyledLink>{" "} <
+  span style = {
+    {
+      color: "#bbb"
+    }
+  } >
+  —{
+    frontmatter.date
+  } <
+  /span> <
+  /PostHeader> {
+    frontmatter.image ? ( <
+      Image src = {
+        frontmatter.image
+      }
+      fit = "cover" / >
+    ) : ( <
+      p > {
+        excerpt
+      } < /p>
+    )
+  } <
+  /Box>
 );
 
-export const query = graphql`
+export const query = graphql `
   query {
     allMdx(
       filter: { fields: { isPublic: { eq: true } } }
@@ -132,7 +185,7 @@ export const query = graphql`
           }
           frontmatter {
             title
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "DD MMMM, YYYY", locale: "es")
             image
           }
           excerpt
