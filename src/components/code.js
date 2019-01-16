6cda004a8c78d2f37bbffd8a1d2737ebc6f18722
artmadeit <this.arthur.email@gmail.com>
2019-01-15 19:58:56 -0500

    feat: <Code> don't delete non blank code line

--- src/components/code.js
+++ src/components/code.js
@@ -4,19 +4,27 @@ import { github } from "react-syntax-highlighter/dist/styles/hljs";
 
 const codeTheme = github;
 
+/**
+ * Use it <Code language="javascript">{`const a = b + c`}</Code>
+ * @see lenguajes-innovadores.mdx for Component syntax
+ * @see arquitectura-hexagonal.mdx for markdown syntax (like normal .md)
+ */
 export default ({ children, language }) => (
-  <SyntaxHighlighter style={codeTheme}>
+  <SyntaxHighlighter style={codeTheme} language={language}>
     {removeSpaces(children)}
   </SyntaxHighlighter>
 );
 
 const removeSpaces = code => {
-  const lines = code.split("\n");
-  const result = lines.slice(1); // real first line with code is 1
+  const lines = getLines(code);
+  const spaces = spacesOnLeft(lines[0]);
+  return lines.map(line => line.substring(spaces, line.length)).join("\n");
+};
 
-  const firstLineWithCode = result[0];
-  const spaces = spacesOnLeft(firstLineWithCode);
-  return result.map(line => line.substring(spaces, line.length)).join("\n");
+const getLines = code => {
+  const lines = code.split("\n");
+  const firstLineWithCodeIndex = isBlank(lines[0]) ? 1 : 0;
+  return lines.slice(firstLineWithCodeIndex);
 };
 
 const spacesOnLeft = str => str.match(/^ */)[0].length;
