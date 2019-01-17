f932f8055c540e03a2908162dc532ee2cba9ba1b
qpdian <qpdiam@gmail.com>
2019-01-16 23:47:03 -0500

    Revert "feat: add style to social links"
    This reverts commit 1179d93bf3eec717ad0ccb5e121343787b52d83a.

--- src/components/posts/social-links.js
+++ src/components/posts/social-links.js
@@ -1,38 +1,37 @@
 import React from "react";
 import {
-    FacebookShareButton,
-    TwitterShareButton,
-    LinkedinShareButton,
-    FacebookIcon,
-    TwitterIcon,
-    LinkedinIcon
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedinShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon
 } from "react-share";
-import { Box } from "grommet";
+
 export default ({ title, description, url }) => (
-    <Box
-        pad="small"
-        fill="true"
-        direction="row"
-        align="center"
-        justify="center"
-        gap="small"
-    >
+  <div>
+    <ul>
+      <li>
         <FacebookShareButton url={url} quote={title}>
-            <FacebookIcon size={32} round />
+          <FacebookIcon size={32} round />
         </FacebookShareButton>
-
+      </li>
+      <li>
         <TwitterShareButton url={url} title={title}>
-            <TwitterIcon size={32} round />
+          <TwitterIcon size={32} round />
         </TwitterShareButton>
-
+      </li>
+      <li>
         <LinkedinShareButton
-            url={url}
-            title={title}
-            description={description}
-            windowWidth={750}
-            windowHeight={600}
+          url={url}
+          title={title}
+          description={description}
+          windowWidth={750}
+          windowHeight={600}
         >
-            <LinkedinIcon size={32} round />
+          <LinkedinIcon size={32} round />
         </LinkedinShareButton>
-    </Box>
+      </li>
+    </ul>
+  </div>
 );
