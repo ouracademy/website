9a5f7f54750c3535e1cd9f63b8c27a0c8c8b929e
qpdian <qpdiam@gmail.com>
2019-01-16 23:57:09 -0500

    feat: change style to template

--- src/components/posts/social-links.js
+++ src/components/posts/social-links.js
@@ -1,30 +1,30 @@
 import React from "react";
 import {
-  FacebookShareButton,
-  TwitterShareButton,
-  LinkedinShareButton,
-  FacebookIcon,
-  TwitterIcon,
-  LinkedinIcon
+    FacebookShareButton,
+    TwitterShareButton,
+    LinkedinShareButton,
+    FacebookIcon,
+    TwitterIcon,
+    LinkedinIcon
 } from "react-share";
 import { Box } from "grommet";
 
 export default ({ title, description, url }) => (
-  <Box direction="row" gap="small">
-    <FacebookShareButton url={url} quote={title}>
-      <FacebookIcon size={32} round />
-    </FacebookShareButton>
-    <TwitterShareButton url={url} title={title}>
-      <TwitterIcon size={32} round />
-    </TwitterShareButton>
-    <LinkedinShareButton
-      url={url}
-      title={title}
-      description={description}
-      windowWidth={750}
-      windowHeight={600}
-    >
-      <LinkedinIcon size={32} round />
-    </LinkedinShareButton>
-  </Box>
+    <Box direction="row" pad="medium" gap="small" fill="true" justify="center">
+        <FacebookShareButton url={url} quote={title}>
+            <FacebookIcon size={40} round />
+        </FacebookShareButton>
+        <TwitterShareButton url={url} title={title}>
+            <TwitterIcon size={40} round />
+        </TwitterShareButton>
+        <LinkedinShareButton
+            url={url}
+            title={title}
+            description={description}
+            windowWidth={750}
+            windowHeight={600}
+        >
+            <LinkedinIcon size={40} round />
+        </LinkedinShareButton>
+    </Box>
 );
--- src/components/posts/template.js
+++ src/components/posts/template.js
@@ -12,91 +12,96 @@ import { Card } from "../../pages/about";
 import DesignSystem from "../design-system";
 
 const Tags = ({ tags }) => (
-  <Box pad="small" direction="row" align="center" gap="small" wrap>
-    <TagIcon />
-    {tags.map(tag => (
-      <Tag key={tag} tag={tag} />
-    ))}
-  </Box>
+    <Box pad="small" direction="row" align="center" gap="small" wrap>
+        <TagIcon />
+        {tags.map(tag => (
+            <Tag key={tag} tag={tag} />
+        ))}
+    </Box>
 );
 
 const Tag = ({ tag }) => (
-  <Link to={`/tags/${tag}`}>
-    <Button hoverIndicator="light-1" onClick={() => {}}>
-      <Box pad="small" direction="row" align="center" gap="small">
-        <Text>{tag}</Text>
-      </Box>
-    </Button>
-  </Link>
+    <Link to={`/tags/${tag}`}>
+        <Button hoverIndicator="light-1" onClick={() => {}}>
+            <Box pad="small" direction="row" align="center" gap="small">
+                <Text>{tag}</Text>
+            </Box>
+        </Button>
+    </Link>
 );
 
 const Author = ({ name, avatar, description }) => (
-  <Card name={name} avatar={avatar} description={description} />
+    <Card name={name} avatar={avatar} description={description} />
 );
 
 const PostPageTemplate = ({ data: { mdx }, location }) => {
-  const { id, code, excerpt: description } = mdx;
-  const { title, author, tags, image = null } = mdx.frontmatter;
-  const url = location.href;
+    const { id, code, excerpt: description } = mdx;
+    const { title, author, tags, image = null } = mdx.frontmatter;
+    const url = location.href;
 
-  return (
-    <Layout
-      description={description}
-      title={title}
-      image={image}
-      type="article"
-    >
-      <Box as="article">
-        <Heading>{title}</Heading>
-        <SocialLinks title={title} description={description} url={url} />
-        <MDXRenderer
-          components={{
-            pre: DesignSystem.Code
-          }}
+    return (
+        <Layout
+            description={description}
+            title={title}
+            image={image}
+            type="article"
         >
-          {code.body}
-        </MDXRenderer>
-        <Tags tags={tags} />
-        <Author {...author} />
-      </Box>
-      <Comments id={id} title={title} url={url} />
-    </Layout>
-  );
+            <Box as="article">
+                <Heading>{title}</Heading>
+
+                <MDXRenderer
+                    components={{
+                        pre: DesignSystem.Code
+                    }}
+                >
+                    {code.body}
+                </MDXRenderer>
+                <SocialLinks
+                    title={title}
+                    description={description}
+                    url={url}
+                />
+                <Tags tags={tags} />
+                <Author {...author} />
+            </Box>
+            <Comments id={id} title={title} url={url} />
+        </Layout>
+    );
 };
 
 const disqusShortname = "academyforus";
 
 const Comments = ({ id, url, title }) => (
-  <DiscussionEmbed
-    shortname={disqusShortname}
-    config={{
-      url,
-      identifier: id,
-      title
-    }}
-  />
+    <DiscussionEmbed
+        shortname={disqusShortname}
+        config={{
+            url,
+            identifier: id,
+            title
+        }}
+    />
 );
 
 export default PostPageTemplate;
 
 export const query = graphql`
-  query($id: String!) {
-    mdx(id: { eq: $id }) {
-      id
-      frontmatter {
-        title
-        author {
-          avatar
-          name
-          description
+    query($id: String!) {
+        mdx(id: { eq: $id }) {
+            id
+            frontmatter {
+                title
+                author {
+                    avatar
+                    name
+                    description
+                }
+                tags
+                image
+            }
+            excerpt
+            code {
+                body
+            }
         }
-        tags
-        image
-      }
-      excerpt
-      code {
-        body
-      }
     }
-  }
 `;
