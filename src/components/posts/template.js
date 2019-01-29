513bc8550e0ef249a585a7346f03e40accd90ae8
artmadeit <this.arthur.email@gmail.com>
2019-01-28 22:27:37 -0500

    fix: wrong url tag

--- src/components/posts/template.js
+++ src/components/posts/template.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import MDXRenderer from "gatsby-mdx/mdx-renderer";
 import { DiscussionEmbed } from "disqus-react";
-import { Box, Button, Heading, Text } from "grommet";
+import { Box, Heading } from "grommet";
 import { Tag as TagIcon } from "grommet-icons";
+import { Tag } from "../../pages/tags";
 
 import Layout from "../layout";
 import SocialLinks from "./social-links";
@@ -12,96 +13,82 @@ import { Card } from "../../pages/about";
 import DesignSystem from "../design-system";
 
 const Tags = ({ tags }) => (
-    <Box pad="small" direction="row" align="center" gap="small" wrap>
-        <TagIcon />
-        {tags.map(tag => (
-            <Tag key={tag} tag={tag} />
-        ))}
-    </Box>
-);
-
-const Tag = ({ tag }) => (
-    <Link to={`/tags/${tag}`}>
-        <Button hoverIndicator="light-1" onClick={() => {}}>
-            <Box pad="small" direction="row" align="center" gap="small">
-                <Text>{tag}</Text>
-            </Box>
-        </Button>
-    </Link>
+  <Box pad="small" direction="row" align="center" gap="small" wrap>
+    <TagIcon />
+    {tags.map(tag => (
+      <Tag key={tag} name={tag} />
+    ))}
+  </Box>
 );
 
 const Author = ({ name, avatar, description }) => (
-    <Card name={name} avatar={avatar} description={description} />
+  <Card name={name} avatar={avatar} description={description} />
 );
 
 const PostPageTemplate = ({ data: { mdx }, location }) => {
-    const { id, code, excerpt: description } = mdx;
-    const { title, author, tags, image = null } = mdx.frontmatter;
-    const url = location.href;
+  const { id, code, excerpt: description } = mdx;
+  const { title, author, tags, image = null } = mdx.frontmatter;
+  const url = location.href;
 
-    return (
-        <Layout
-            description={description}
-            title={title}
-            image={image}
-            type="article"
+  return (
+    <Layout
+      description={description}
+      title={title}
+      image={image}
+      type="article"
+    >
+      <Box as="article">
+        <Heading>{title}</Heading>
+
+        <MDXRenderer
+          components={{
+            pre: DesignSystem.Code
+          }}
         >
-            <Box as="article">
-                <Heading>{title}</Heading>
-
-                <MDXRenderer
-                    components={{
-                        pre: DesignSystem.Code
-                    }}
-                >
-                    {code.body}
-                </MDXRenderer>
-                <SocialLinks
-                    title={title}
-                    description={description}
-                    url={url}
-                />
-                <Tags tags={tags} />
-                <Author {...author} />
-            </Box>
-            <Comments id={id} title={title} url={url} />
-        </Layout>
-    );
+          {code.body}
+        </MDXRenderer>
+        <SocialLinks title={title} description={description} url={url} />
+        <Tags tags={tags} />
+        <Author {...author} />
+      </Box>
+      <Comments id={id} title={title} url={url} />
+    </Layout>
+  );
 };
 
 const disqusShortname = "academyforus";
 
 const Comments = ({ id, url, title }) => (
-    <DiscussionEmbed
-        shortname={disqusShortname}
-        config={{
-            url,
-            identifier: id,
-            title
-        }}
-    />
+  <DiscussionEmbed
+    shortname={disqusShortname}
+    config={{
+      url,
+      identifier: id,
+      title
+    }}
+  />
 );
 
 export default PostPageTemplate;
 
 export const query = graphql`
-    query($id: String!) {
-        mdx(id: { eq: $id }) {
-            id
-            frontmatter {
-                title
-                author {
-                    avatar
-                    name
-                    description
-                }
-                tags
-                image
-            }
-            excerpt
-            code {
-                body
-            }
+  query($id: String!) {
+    mdx(id: { eq: $id }) {
+      id
+      frontmatter {
+        title
+        author {
+          avatar
+          name
+          description
         }
+        tags
+        image
+      }
+      excerpt
+      code {
+        body
+      }
     }
+  }
 `;
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,54 +1,63 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
-import { Box } from "grommet";
+import { Box, Button, Text } from "grommet";
 import { Tag as TagIcon } from "grommet-icons";
 import styled from "styled-components";
 import Layout from "../components/layout";
 import kebabCase from "lodash/kebabCase";
 
 const TagQuantity = styled.div`
-    background: #ababab;
-    width: 25px;
-    color: white;
-    border: 10%;
-    text-align: center;
+  background: #ababab;
+  width: 25px;
+  color: white;
+  border: 10%;
+  text-align: center;
 `;
 
+const Container = ({ children }) => (
+  <Box direction="row" align="center" gap="small">
+    {children}
+  </Box>
+);
+
+export const Tag = ({ name, children }) => (
+  <Link to={`/tags/${kebabCase(name)}/`}>
+    <Button hoverIndicator="light-1" onClick={() => {}}>
+      <Container>
+        <Text>{name}</Text>
+        {children}
+      </Container>
+    </Button>
+  </Link>
+);
+
 export default ({
-    data: {
-        allMdx: { group }
-    }
+  data: {
+    allMdx: { group }
+  }
 }) => (
-    <Layout title="Tags">
-        <h1>Tags</h1>
-        <Box direction="column" gap="small">
-            {group.map(tag => (
-                <Box
-                    direction="row"
-                    align="center"
-                    gap="small"
-                    key={tag.fieldValue}
-                >
-                    <TagIcon />
-                    <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                        <Box direction="row" align="center" gap="small">
-                            {tag.fieldValue}
-                            <TagQuantity>{tag.totalCount}</TagQuantity>
-                        </Box>
-                    </Link>
-                </Box>
-            ))}
-        </Box>
-    </Layout>
+  <Layout title="Tags">
+    <Container>
+      <TagIcon />
+      <h1>Tags</h1>
+    </Container>
+    <Box gap="small">
+      {group.map(tag => (
+        <Tag name={tag.fieldValue} key={tag.fieldValue}>
+          <TagQuantity>{tag.totalCount}</TagQuantity>
+        </Tag>
+      ))}
+    </Box>
+  </Layout>
 );
 
 export const pageQuery = graphql`
-    query {
-        allMdx(limit: 2000, filter: { fields: { isPublic: { eq: true } } }) {
-            group(field: frontmatter___tags) {
-                fieldValue
-                totalCount
-            }
-        }
+  query {
+    allMdx(limit: 2000, filter: { fields: { isPublic: { eq: true } } }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
     }
+  }
 `;
