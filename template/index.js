74fe7ee16592ddddb27acb786b1918c1f7c1cacd
artmadeit <this.arthur.email@gmail.com>
2019-08-07 21:51:25 -0500

    feat: initial command line for creating a post

---
+++ template/index.js
@@ -0,0 +1,68 @@
+const Mustache = require("mustache");
+
+const slugify = require("slugify");
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+
+const slug = text => slugify(text).toLowerCase();
+const getPostPath = text => path.join("src/posts", slug(text) + ".mdx");
+
+console.log("Hi! We'll help you write a post");
+inquirer
+  .prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "Enter it's title:"
+    },
+    {
+      name: "description",
+      type: "input",
+      message:
+        "Describe it but in an engaging way to people - please don't exceed 160 characters, this is for SEO purposes 😉. \n" +
+        "More on https://www.seoclarity.net/resources/knowledgebase/write-perfect-meta-description-seo-17115/ \n"
+    },
+    {
+      name: "author",
+      type: "input",
+      message: "Identify you, by putting your author id (see authors.yml)"
+    }
+  ])
+  .then(answers => {
+    console.log(answers);
+
+    const format = require("date-fns/format");
+    const view = {
+      title: answers.title,
+      description: answers.description,
+      date: format(new Date(), "yyyy-MM-dd"),
+      author: answers.author,
+      tags: ["historia", "personajes del software", "desarrollo de software"],
+      image: "https://pbs.twimg.com/media/D-ZG3OWXsAAruSS?format=png&name=small"
+    };
+
+    fs.writeFile(getPostPath(view.title), newPost(view), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log("Post create, write your post 😃");
+    });
+  });
+
+const newPost = view =>
+  Mustache.render(
+    `---
+title: {{title}}
+image: {{imageURL}}
+date: {{date}}
+author: {{author}}
+tags: [tag-1, tag-2]
+description: some desc # optional, default to an excerpt of the post
+---
+
+Your content in markdown but with the power of JSX. MDX!
+`,
+    view
+  );
