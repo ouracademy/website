f82e7c94b45e030a7a79d78059549da645a9c622
qpdian <qpdiam@gmail.com>
2020-05-09 16:23:54 -0500

    feat: white card

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,109 +1,114 @@
-import React from "react";
-import Layout from "../components/layout";
-import styled from "styled-components";
 import { graphql, Link } from "gatsby";
-
 import { Box, Image } from "grommet";
+import React from "react";
+import styled from "styled-components";
+import Layout from "../components/layout";
 import { Tags } from "../components/tags/tags";
 
 const Grid = styled.div`
-  display: grid;
-  grid-gap: 5px;
-  margin: 2vh 0;
-
-  & div {
-    height: 330px;
-  }
-
-  @media (min-width: 600px) {
-    grid-template-columns: repeat(10, 1fr);
+    display: grid;
+    grid-gap: 5px;
+    margin: 2vh 0;
 
     & div {
-      height: 400px;
+        height: 330px;
     }
 
-    & div:nth-child(4n + 1) {
-      grid-column: span 6;
-    }
+    @media (min-width: 600px) {
+        grid-template-columns: repeat(10, 1fr);
 
-    & div:nth-child(4n + 2) {
-      grid-column: span 4;
-    }
-    & div:nth-child(4n + 3) {
-      grid-column: span 5;
-    }
+        & div {
+            height: 400px;
+        }
 
-    & div:nth-child(4n) {
-      grid-column: span 5;
+        & div:nth-child(4n + 1) {
+            grid-column: span 6;
+        }
+
+        & div:nth-child(4n + 2) {
+            grid-column: span 4;
+        }
+        & div:nth-child(4n + 3) {
+            grid-column: span 5;
+        }
+
+        & div:nth-child(4n) {
+            grid-column: span 5;
+        }
     }
-  }
 `;
 
 const PostHeader = styled.h3`
-  margin-bottom: 1rem;
+    margin-bottom: 1rem;
 `;
 
 const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: inherit;
+    text-decoration: none;
+    color: inherit;
 `;
 
 const PostTitle = styled.span`
-  font-size: 1.5rem;
+    font-size: 1.5rem;
 `;
 
 export const PostItem = ({ fields, frontmatter, excerpt }) => (
-  <Box elevation="medium" pad="medium">
-    <PostHeader>
-      <StyledLink to={`/posts${fields.slug}`}>
-        <PostTitle> {frontmatter.title} </PostTitle>
-      </StyledLink>
-      <span
+    <Box
+        elevation="medium"
+        pad="medium"
         style={{
-          color: "#bbb"
+            background: "white"
         }}
-      >
-        {frontmatter.date && `—${frontmatter.date}`}
-      </span>
-    </PostHeader>
-    {frontmatter.image ? (
-      <Image src={frontmatter.image} fit="cover" />
-    ) : (
-      <p> {frontmatter.description || excerpt} </p>
-    )}
-  </Box>
+    >
+        <PostHeader>
+            <StyledLink to={`/posts${fields.slug}`}>
+                <PostTitle> {frontmatter.title} </PostTitle>
+            </StyledLink>
+            <span
+                style={{
+                    color: "#bbb"
+                }}
+            >
+                {frontmatter.date && `—${frontmatter.date}`}
+            </span>
+        </PostHeader>
+        {frontmatter.image ? (
+            <Image src={frontmatter.image} fit="cover" />
+        ) : (
+            <p> {frontmatter.description || excerpt} </p>
+        )}
+    </Box>
 );
 
 export default ({ data: { allMdx } }) => (
-  <Layout>
-    <h1>Articulos</h1>
-    <Box margin="small">
-      <Tags />
-    </Box>
-    <Posts posts={allMdx.edges} />
-  </Layout>
+    <Layout>
+        <h1>Articulos</h1>
+        <Box margin="small">
+            <Tags />
+        </Box>
+        <Posts posts={allMdx.edges} />
+    </Layout>
 );
 
 export const Posts = ({ posts }) => (
-  <Grid>
-    {posts.map(({ node }) => (
-      <PostItem key={node.id} {...node} />
-    ))}
-  </Grid>
+    <Grid>
+        {posts.map(({ node }) => (
+            <PostItem key={node.id} {...node} />
+        ))}
+    </Grid>
 );
 
 export const query = graphql`
-  query {
-    allMdx(
-      filter: { fields: { isPublic: { eq: true } } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      totalCount
-      edges {
-        node {
-          ...PostInfo
+    query {
+        allMdx(
+            filter: { fields: { isPublic: { eq: true } } }
+            sort: { fields: [frontmatter___date], order: DESC }
+        ) {
+            totalCount
+            edges {
+                node {
+                    ...PostInfo
+                }
+            }
         }
-      }
     }
-  }
 `;
